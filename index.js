8dd5106ccd06caefa860101b74f1917a48005dce
bakon <bakon@bakon.dev>
2021-03-10 01:47:24 -0600

    small eye candy fixes

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const bme280 = require('bme280sensor');
 bme280.open(0x76).then(async sensor => {
   const sensor1 = await sensor.read();
   console.log(
-    "Sensor 1 Temp: " + sensor1.temperature.toFixed(2) +"C/n" +
-    "Sensor 1 Humid: " + sensor1.humidity.toFixed(2) +"%/n" +
-    "Sensor 1 pressure: " + sensor1.pressure.toFixed(2) +"%/n"
+    "####Sensor 1#### \n" + 
+    "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
+    "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
+    "Pressure: " + sensor1.pressure.toFixed(2) +"%\n"
   );
   await sensor.close();
 }).catch(console.log);
@@ -13,9 +14,10 @@ bme280.open(0x76).then(async sensor => {
 bme280.open(0x77).then(async sensor => {
   const sensor1 = await sensor.read();
   console.log(
-    "Sensor 2 Temp: " + sensor1.temperature.toFixed(2) +"C/n" +
-    "Sensor 2 Humid: " + sensor1.humidity.toFixed(2) +"%/n" +
-    "Sensor 2 pressure: " + sensor1.pressure.toFixed(2) +"%/n"
+    "####Sensor 2#### \n" + 
+    "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
+    "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
+    "Pressure: " + sensor1.pressure.toFixed(2) +"%\n"
   );
   await sensor.close();
 }).catch(console.log);
