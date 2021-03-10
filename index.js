e39bde3f0d9ed67541c4982a8fe04bbe955396d8
bakon <bakon@bakon.dev>
2021-03-10 13:13:17 -0600

    interval stuff

--- index.js
+++ index.js
@@ -8,21 +8,24 @@ const CtoF = (c) =>{
 
 const getSensorData = (sensor) => {
   const sensorID = sensor.i2cAddress.toString(16);
-  bme280.open(sensor).then(async sensor => {
-    const sensor1 = await sensor.read();
-    console.log(
-      "####Sensor "+ sensorID +"#### \n" + 
-      "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
-      "Temp: " + CtoF(sensor1.temperature.toFixed(2)) + "F\n" +
-      "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
-      "Pressure: " + sensor1.pressure.toFixed(2) +"hPa\n"
-    );
-    makeGraph();
-    await sensor.close();
-  }).catch(console.log);
+  let count;
+  setInterval(()=>{
+    bme280.open(sensor).then(async sensor => {
+      const sensor1 = await sensor.read();
+      console.log(
+        "####Sensor "+ sensorID +"#### \n" + 
+        "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
+        "Temp: " + CtoF(sensor1.temperature.toFixed(2)) + "F\n" +
+        "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
+        "Pressure: " + sensor1.pressure.toFixed(2) +"hPa\n"
+      );
+      await makeGraph();
+      await sensor.close();
+    }).catch(console.log);
+  }, 2000);
 };
 
-const makeGraph = () =>{
+const makeGraph = async () =>{
   var s0 = new Array (120)
   for (var i = 0; i < s0.length; i++)
       s0[i] = 15 * Math.sin (i * ((Math.PI * 4) / s0.length))
