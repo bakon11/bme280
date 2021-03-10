ecd417af36e13c94ad8c62547c4b7bdd3fae6cbe
bakon <maciej@geoskygps.com>
2021-03-10 21:13:29 +0000

    some graph fun

--- index.js
+++ index.js
@@ -8,29 +8,40 @@ const CtoF = (c) =>{
 
 const getSensorData = (sensor) => {
   const sensorID = sensor.i2cAddress.toString(16);
-  let count;
+  let temp = [];
+  let humid = [];	 
   setInterval(()=>{
     bme280.open(sensor).then(async sensor => {
-      const sensor1 = await sensor.read();
-      console.log(
-        "####Sensor "+ sensorID +"#### \n" + 
-        "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
-        "Temp: " + CtoF(sensor1.temperature.toFixed(2)) + "F\n" +
-        "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
-        "Pressure: " + sensor1.pressure.toFixed(2) +"hPa\n"
-      );
-      await makeGraph();
+      const sensorData = await sensor.read();
+      temp.push(sensorData.temperature);
+      humid.push(sensorData.humidity);
+      console.log("######################################################################################");	    
+      await displayData(sensorID, sensorData);     
+      await makeGraph("Temperature", "C*", temp);
+      await makeGraph("Humidity", "%", humid);
+      console.log("######################################################################################");   
       await sensor.close();
     }).catch(console.log);
   }, 2000);
 };
 
-const makeGraph = async () =>{
-  var s0 = new Array (120)
-  for (var i = 0; i < s0.length; i++)
-      s0[i] = 15 * Math.sin (i * ((Math.PI * 4) / s0.length))
-  console.log (asciichart.plot (s0))
+const displayData = (sensorID, sensorData) => {
+ console.log(
+        "####Sensor "+ sensorID +"#### \n" + 
+        "Temp: " + sensorData.temperature.toFixed(2) + "C | " + CtoF(sensorData.temperature.toFixed(2)) +"F\n" +
+        "Humid: " + sensorData.humidity.toFixed(2) +"%\n" +
+        "Pressure: " + sensorData.pressure.toFixed(2) +"hPa\n"
+      );
+};
+
+const makeGraph = (sensorName, measurement, sensorInfo) => {
+ console.log("########"+sensorName+"########\n");
+ console.log("Max:"+Math.max(...sensorInfo).toFixed(2)+ measurement +  
+	     " | Min:"+Math.min(...sensorInfo).toFixed(2) + measurement + 
+	     " | Avg:"+(sensorInfo.reduce((a,b) => a + b, 0) / sensorInfo.length).toFixed(2) + measurement + "\n");	
+ console.log(asciichart.plot (sensorInfo, { height: 6 }) + "\n") // this rescales the graph to ±3 lines 
 };
 
 getSensorData({i2cAddress: 0x76});
 getSensorData({i2cAddress: 0x77});
+//
