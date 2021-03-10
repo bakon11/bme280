aaf67ca121b7c43aa6663729ae66df59d2d72065
bakon <bakon@bakon.dev>
2021-03-10 12:19:57 -0600

    added F conversion

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const bme280 = require('bme280sensor');
 
+const CtoF = (c) =>{
+  const F = (c * (9/5) + 32);
+  return F;
+};
+
+
 bme280.open({i2cAddress: 0x76}).then(async sensor => {
   const sensor1 = await sensor.read();
   console.log(
     "####Sensor 1#### \n" + 
     "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
+    "Temp: " + CtoF(sensor1.temperature.toFixed(2)) + "F\n" +
     "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
     "Pressure: " + sensor1.pressure.toFixed(2) +"hPa\n"
   );
@@ -16,6 +23,7 @@ bme280.open({i2cAddress: 0x77}).then(async sensor => {
   console.log(
     "####Sensor 2#### \n" + 
     "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
+    "Temp: " + CtoF(sensor1.temperature.toFixed(2)) + "F\n" +
     "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
     "Pressure: " + sensor1.pressure.toFixed(2) +"hPa\n"
   );
