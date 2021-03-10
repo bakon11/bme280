1195df17e750101b357367d019a644f36eac5f9e
bakon <bakon@bakon.dev>
2021-03-10 01:12:44 -0600

    upodated with multi sensor support package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const bme280 = require('bme280');
+const bme280 = require('bme280sensor');
 
-bme280.open().then(async sensor => {
+bme280.open(0x77).then(async sensor => {
   console.log(await sensor.read());
   await sensor.close();
 }).catch(console.log);
