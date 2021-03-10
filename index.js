f52811c212131a2302acdf74dcaa2d279ac2855b
bakon <bakon@bakon.dev>
2021-03-10 01:42:32 -0600

    display both  sensores

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const bme280 = require('bme280sensor');
 
-bme280.open(0x77).then(async sensor => {
-  console.log(await sensor.read());
+bme280.open(0x76).then(async sensor => {
+  const sensor1 = await sensor.read();
+  console.log(
+    "Sensor 1 Temp: " + sensor1.temperature.toFixed(2) +"C/n" +
+    "Sensor 1 Humid: " + sensor1.humidity.toFixed(2) +"%/n" +
+    "Sensor 1 pressure: " + sensor1.pressure.toFixed(2) +"%/n"
+  );
   await sensor.close();
 }).catch(console.log);
 
+bme280.open(0x77).then(async sensor => {
+  const sensor1 = await sensor.read();
+  console.log(
+    "Sensor 2 Temp: " + sensor1.temperature.toFixed(2) +"C/n" +
+    "Sensor 2 Humid: " + sensor1.humidity.toFixed(2) +"%/n" +
+    "Sensor 2 pressure: " + sensor1.pressure.toFixed(2) +"%/n"
+  );
+  await sensor.close();
+}).catch(console.log);
