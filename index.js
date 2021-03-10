bf4d011b5535b0edf38e45cb65be9a31d340bbaf
bakon <bakon@bakon.dev>
2021-03-10 12:44:49 -0600

    Make functional

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const CtoF = (c) =>{
 
 const getSensorData = (sensor) => {
   console.log(sensor.i2cAddress);
+  const sensorID = sensor.i2cAddress;
   bme280.open(sensor).then(async sensor => {
     const sensor1 = await sensor.read();
     console.log(
-      "####Sensor_"+ sensor.i2cAddress +"#### \n" + 
+      "####Sensor_"+ sensorID +"#### \n" + 
       "Temp: " + sensor1.temperature.toFixed(2) +"C\n" +
       "Temp: " + CtoF(sensor1.temperature.toFixed(2)) + "F\n" +
       "Humid: " + sensor1.humidity.toFixed(2) +"%\n" +
